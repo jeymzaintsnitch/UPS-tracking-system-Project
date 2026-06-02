abbb6a1168e52710eae85e5b4381825b5e1e2dc9
James Rolmer Catalan <jamescatalan19@gmail.com>
2026-06-03 01:44:52 +0800

    Delete webdev1 directory

--- webdev1/template/body.blade.php
+++
@@ -1,6 +0,0 @@
-
-@extends('template.main')
-@section('content')
-    <h1>Welcome to My App</h1>
-    @section('title', 'welcome to blade ')
-@endsection
--- webdev1/template/compute.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('template.main')
-@section('content') 
-    
-    <h1>Sum: {{$sum}}</h1>
-    <h1>Difference: {{ $diff}}</h1>
-    <h1>Product: {{ $multiply }}</h1>
-    <h1>Quotient: {{$divide}}</h1>
-
-@endsection
